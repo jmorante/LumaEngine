f78e6cc1dea1e17a35d837189289d3884da6ac91
Jared Morante <jmorante@gmail.com>
2020-08-09 22:03:44 -0700

    Move project to a proper mac build.

---
+++ Pipeline/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pipeline
+//
+//  Created by Jared Morante on 8/9/20.
+//  Copyright © 2020 Jared Morante. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Pipeline/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Pipeline
+//
+//  Created by Jared Morante on 8/9/20.
+//  Copyright © 2020 Jared Morante. All rights reserved.
+//
+
+import Cocoa
+import MetalKit
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        guard let metalView = view as? MTKView else {
+            fatalError("metal view not set up in storyboard")
+        }
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
