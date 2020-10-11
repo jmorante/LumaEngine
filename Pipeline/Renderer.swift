6529873c99a74f155868ceb3ed50aa4e1aaf1348
Jared Morante <jmorante@gmail.com>
2020-10-11 15:53:24 -0700

    Add defaults to the renderer class, making sure the metal view will set the renderer as it's delegate.

--- Pipeline/Renderer.swift
+++ Pipeline/Renderer.swift
@@ -9,8 +9,27 @@
 import MetalKit
 
 class Renderer: NSObject {
+    static var device: MTLDevice!
+    static var commandQueue: MTLCommandQueue!
+    var mesh: MTKMesh!
+    var vertexBuffer: MTLBuffer!
+    var pipelineState: MTLRenderPipelineState!
+    
     init(metalView: MTKView) {
+        guard let device = MTLCreateSystemDefaultDevice(),
+              let commandQueue = device.makeCommandQueue()
+        else {
+            fatalError("GPU not available")
+        }
+        
+        Renderer.device = device
+        Renderer.commandQueue = commandQueue
+        metalView.device = device
+        
         super.init()
+        
+        metalView.clearColor = MTLClearColor(red: 1.0, green: 1.0, blue: 0.8, alpha: 1.0)
+        metalView.delegate = self
     }
 }
 
