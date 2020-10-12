b8ceb3c6ab64177bda093eaa6a1b4e08cbb83e77
Jared Morante <jmorante@gmail.com>
2020-10-11 17:14:00 -0700

    Setup potential state for the GPU.

--- Pipeline/Renderer.swift
+++ Pipeline/Renderer.swift
@@ -26,6 +26,33 @@ class Renderer: NSObject {
         Renderer.commandQueue = commandQueue
         metalView.device = device
         
+        let mdlMesh = Primitive.makeCube(device: device, size: 1)
+        do {
+            mesh = try MTKMesh(mesh: mdlMesh, device: device)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        vertexBuffer = mesh.vertexBuffers[0].buffer
+        
+        let library = device.makeDefaultLibrary()
+        let vertexFunction = library?.makeFunction(name: "vertex_main")
+        let fragmentFunction = library?.makeFunction(name: "fragment_main")
+        
+        // create the pipeline state
+        let pipelineDescriptor = MTLRenderPipelineDescriptor()
+        pipelineDescriptor.vertexFunction = vertexFunction
+        pipelineDescriptor.fragmentFunction = fragmentFunction
+        pipelineDescriptor.vertexDescriptor = MTKMetalVertexDescriptorFromModelIO(mdlMesh.vertexDescriptor)
+        pipelineDescriptor.colorAttachments[0].pixelFormat = metalView.colorPixelFormat
+        
+        do {
+            pipelineState =
+                try device.makeRenderPipelineState(descriptor: pipelineDescriptor)
+        } catch let error {
+            fatalError(error.localizedDescription)
+        }
+        
         super.init()
         
         metalView.clearColor = MTLClearColor(red: 1.0, green: 1.0, blue: 0.8, alpha: 1.0)
